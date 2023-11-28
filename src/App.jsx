81da470a3e121e0973d5a3c4336022dadbc088d5
devham236 <90037750+devham236@users.noreply.github.com>
2023-11-28 12:57:52 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { apiKey } from "./Utils/apiKey"
+
+const App = () => {
+  return <div className="w-[100vw] h-[100vh]">App</div>
+}
+
+export default App
---
+++ src/Components/MainDiv.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const MainDiv = () => {
+  return <div>MainDiv</div>
+}
+
+export default MainDiv
