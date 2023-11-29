aed204061a9f76bb5bc6bc175d9c2c7ff5908cb9
devham236 <90037750+devham236@users.noreply.github.com>
2023-11-29 09:55:12 +0100

    testing openai api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-import React from "react"
+import React, { useState } from "react"
 import { apiKey } from "./Utils/apiKey"
 
 const App = () => {
-  console.log(apiKey)
-  return <div className="w-[100vw] h-[100vh]">App</div>
+  const [input, setInput] = useState("")
+
+  const checkTweet = async () => {
+    const body = {
+      model: "gpt-3.5-turbo",
+      prompt: "Whats is the sentiment of this tweet? " + input,
+      messages: [],
+      temperature: 0,
+      max_tokens: 256,
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  }
+
+  return (
+    <div className="w-[100vw] h-[100vh] bg-slate-300">
+      <input
+        type="text"
+        name=""
+        id=""
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={checkTweet}>Check</button>
+    </div>
+  )
 }
 
 export default App
