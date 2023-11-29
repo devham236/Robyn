9872a60854422e81fdd78326e1e0f09ada9e619a
devham236 <90037750+devham236@users.noreply.github.com>
2023-11-29 11:26:36 +0100

    widths and heights for larger screens

--- src/Components/MainDiv.jsx
+++ src/Components/MainDiv.jsx
@@ -2,8 +2,8 @@ import React from "react"
 
 const MainDiv = () => {
   return (
-    <div className="w-[900px] h-[calc(100%-56px)] mx-auto flex items-center justify-center bg-green-300">
-      <div className="bg-orange-400 p-4">
+    <div className="lg:w-[900px] lg:h-[calc(100%-56px)] mx-auto flex items-center justify-center bg-green-300">
+      <div className="bg-orange-400 w-full py-10 flex flex-col items-center">
         <h1>Text</h1>
         <button>Start</button>
       </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from "react"
 
 const Navbar = () => {
   return (
-    <div className="w-full h-14 bg-slate-700 flex items-center justify-center">
-      <div className="w-[900px] h-full bg-red-500 flex items-center justify-between">
+    <div className="lg:w-full lg:h-14 bg-slate-700 flex items-center justify-center">
+      <div className="lg:w-[900px] h-full bg-red-500 flex items-center justify-between">
         <div>Logo</div>
         <div>Title</div>
         <div>Darkmode</div>
