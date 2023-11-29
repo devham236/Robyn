f27c8f9e71f3230b7b8b68981d9c549e8b7082fb
devham236 <90037750+devham236@users.noreply.github.com>
2023-11-29 10:34:16 +0100

    skeleton design

--- src/Components/MainDiv.jsx
+++ src/Components/MainDiv.jsx
@@ -3,7 +3,10 @@ import React from "react"
 const MainDiv = () => {
   return (
     <div className="w-[900px] h-[calc(100%-56px)] mx-auto flex items-center justify-center bg-green-300">
-      MainDiv
+      <div className="bg-orange-400 p-4">
+        <h1>Text</h1>
+        <button>Start</button>
+      </div>
     </div>
   )
 }
