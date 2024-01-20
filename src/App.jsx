c6485a2522364e74ae2097553fa8813a312f806d
devham236 <90037750+devham236@users.noreply.github.com>
2024-01-20 11:03:13 +0100

    delted html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,7 @@
-import React, { useState } from "react"
-import { apiKey } from "./Utils/apiKey"
-import Navbar from "./Components/Navbar"
-import MainDiv from "./Components/MainDiv"
-import axios from "axios"
+import React from "react"
 
 const App = () => {
-  const [input, setInput] = useState("")
-
-  const checkTweet = async () => {
-    const body = {
-      model: "gpt-3.5-turbo",
-      prompt: "Whats is the sentiment of this tweet? " + input,
-      messages: [],
-      temperature: 0,
-      max_tokens: 256,
-    }
-
-    await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify(body),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-  }
-
-  return (
-    <div className="w-[100vw] h-[100vh] bg-slate-300">
-      <input
-        type="text"
-        name=""
-        id=""
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={checkTweet}>Check</button>
-      <Navbar />
-      <MainDiv />
-    </div>
-  )
+  return <div className="w-[100vw] h-[100vh] bg-slate-300"></div>
 }
 
 export default App
