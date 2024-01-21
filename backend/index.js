9cbcf8ee8c96308c10e46467b8795a23b0b5e5f3
devham236 <90037750+devham236@users.noreply.github.com>
2024-01-21 10:51:07 +0100

    setup port

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
 const express = require("express")
 const cors = require("cors")
+require("dotenv").config()
 
 const server = express()
 server.use(cors())
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server is listening on Port: ${process.env.PORT}`)
+})
