8e636ccd6bae3ae5461df636039457289f6fe6c4
Hamza Mirza <hamza665@hotmail.de>
2024-02-03 11:01:53 +0100

    test

--- src/Components/MainDiv.jsx
+++ src/Components/MainDiv.jsx
@@ -1,7 +1,13 @@
 import React from "react"
 
 const MainDiv = () => {
-  return <div></div>
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore in animi,
+      repellendus saepe eaque quaerat eos numquam soluta illo iste alias
+      quibusdam tempore. Cum expedita aperiam fugiat nemo deleniti excepturi!
+    </div>
+  )
 }
 
 export default MainDiv
