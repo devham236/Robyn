512b12c7496c1c4b6c9ea71567804edb65109a3b
Hamza Mirza <hamza665@hotmail.de>
2024-02-03 11:00:56 +0100

    set up project after reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react"
 
 const App = () => {
-  return <div></div>
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem optio
+      sapiente excepturi fugiat ullam earum nihil! Est, reiciendis ea possimus
+      deleniti minus et corporis dolor reprehenderit quibusdam, sapiente sunt
+      voluptatem.
+    </div>
+  )
 }
 
 export default App
