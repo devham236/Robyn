8f45122044f10b74e1757e882edfed60aadc62f9
Hamza Mirza <hamza665@hotmail.de>
2024-02-04 16:56:22 +0100

    update instructions

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,6 @@ import React from "react"
 const App = () => {
   const array = new Array(4).fill("test")
 
-  /*
-  - Die Array enthält ein object, jedes object hat eine frage, eine id und vielleicht ein type
-  - iterate über die array und render eine Komponente
-  - die komponente enthält die frage und ein input feld
-  */
-
   return (
     <div className="w-[100dvw] h-[100dvh] bg-slate-800">
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem optio
