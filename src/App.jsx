04dfba231bf283e14d30258804d84655d6ff93b7
Hamza Mirza <hamza665@hotmail.de>
2024-02-04 16:41:32 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react"
 
 const App = () => {
+  const array = new Array(4).fill("test")
+
+  console.table(array)
+
   return (
-    <div>
+    <div className="w-[100dvw] h-[100dvh] bg-slate-800">
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem optio
       sapiente excepturi fugiat ullam earum nihil! Est, reiciendis ea possimus
       deleniti minus et corporis dolor reprehenderit quibusdam, sapiente sunt
