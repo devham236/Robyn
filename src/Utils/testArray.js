02842f30644691df93127703d362029129295ffb
Hamza Mirza <hamza665@hotmail.de>
2024-02-13 16:58:48 +0100

    updated array structure

--- src/Utils/testArray.js
+++ src/Utils/testArray.js
@@ -2,18 +2,22 @@ const elements = [
   {
     question: "How are you related?",
     id: 1,
+    type: "Relationship:",
   },
   {
     question: "What is their age?",
     id: 2,
+    type: "Age:",
   },
   {
     question: "What is their gender?",
     id: 3,
+    type: "Gender:",
   },
   {
     question: "What are their interests?",
     id: 4,
+    type: "Interests:",
   },
 ]
 
